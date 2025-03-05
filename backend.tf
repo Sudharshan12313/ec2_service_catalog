650d5622f071378feb08acf45c0b802c8bf57ea9
sumanthreddy0804 <gsumanth48@gmail.com>
2025-03-05 16:14:50 +0530

    files updated

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform{
   backend "s3" {
     bucket         = "nsh-state-new"
-    key            = "dev/EC2/terraform.tfstate"
+    key            = "EC2/terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true
   }
